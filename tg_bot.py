5b3d54989e03ad21f4a0341965f9e86528e08af6
Nikita <nick1tapinyagin@yandex.ru>
2025-03-17 13:36:15 +0300

    add keyboard

--- tg_bot.py
+++ tg_bot.py
@@ -1,18 +1,36 @@
 import os
 
 from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import CallbackContext, Filters, MessageHandler, Updater
+from telegram import ReplyKeyboardMarkup, Update
+from telegram.ext import (CallbackContext, CommandHandler, Filters,
+                          MessageHandler, Updater)
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    custom_keyboard = [
+        ['Новый вопрос', 'Сдаться'],
+        ['Мой счет']
+    ]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Привет! Я бот для викторин!",
+        reply_markup=reply_markup
+    )
 
 
 def echo(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=update.message.text
+    )
 
 
 def main():
     load_dotenv()
     updater = Updater(token=os.environ['TELEGRAM_TOKEN'], use_context=True)
     dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
     echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
     dispatcher.add_handler(echo_handler)
     updater.start_polling()
