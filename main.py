f873da845609356a7dbd796a3b3a49fa0d594257
Nikita <nick1tapinyagin@yandex.ru>
2025-03-17 09:22:05 +0300

    save questions to dictionary

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-with open('questions/ampers11.txt', 'r', encoding='KOI8-R') as file:
-    print(file.read())
+from collections import defaultdict
+
+
+def main():
+    text = ''
+    with open('questions/1vs1200.txt', 'r', encoding='KOI8-R') as file:
+        text = file.read().split('\n\n')
+
+    questions = defaultdict(str)
+    for i, line in enumerate(text):
+        if 'Ответ' in line:
+            questions[text[i-1]] = line
+
+    # for key, value in questions.items():
+    #     print(key, value)
+
+
+if __name__ == '__main__':
+    main()
