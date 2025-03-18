e56224b409d94ae73deb9a8d4242a7c51eec7f96
Nikita <nick1tapinyagin@yandex.ru>
2025-03-18 09:41:34 +0300

    connect to database

--- tg_bot.py
+++ tg_bot.py
@@ -1,5 +1,6 @@
 import os
 
+import redis
 from dotenv import load_dotenv
 from telegram import ReplyKeyboardMarkup, Update
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
@@ -21,21 +22,34 @@ def start(update: Update, context: CallbackContext) -> None:
     )
 
 
-def echo(update: Update, context: CallbackContext):
+def echo(update: Update, context: CallbackContext, db):
     if update.message.text == 'Новый вопрос':
         question = get_question()
         context.bot.send_message(
             chat_id=update.effective_chat.id,
             text=question
         )
+        success = db.set(update.effective_chat.id, question)
+        print(db.get(update.effective_chat.id))
 
 
 def main():
     load_dotenv()
+    db = redis.Redis(
+        host='redis-13330.c15.us-east-1-2.ec2.redns.redis-cloud.com',
+        port=13330,
+        decode_responses=True,
+        username=os.environ['REDIS_USERNAME'],
+        password=os.environ['REDIS_PASSWORD'],
+    )
+
     updater = Updater(token=os.environ['TELEGRAM_TOKEN'], use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    echo_handler = MessageHandler(
+        Filters.text & (~Filters.command),
+        lambda update, context: echo(update, context, db)
+    )
     dispatcher.add_handler(echo_handler)
     updater.start_polling()
 
